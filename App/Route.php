bf98d8a0f7cd031dfac00eedc34f7d65e685237a
thiagoalmeidadon <thiagodons@gmail.com>
2021-05-18 20:53:57 -0300

    abstracao Route

--- App/Route.php
+++ App/Route.php
@@ -2,24 +2,14 @@
 
   namespace App;
 
-  class Route {
+  use MF\Init\Bootstrap;
 
-  	private $routes;
+  class Route extends Bootstrap {
 
-  	public function __construct(){
-  		$this->initRoutes();
-  		$this->run($this->getUrl());
-  	}
+  	
 
-  	public function getRoutes(){
-  		return $this->routes;
-  	}
+  	protected function initRoutes() {
 
-  	public function setRoutes(array $routes){
-  		$this->routes = $routes;
-  	}
-
-  	public function initRoutes() {
   		$routes['home'] = array(
   			'route' => '/',
   			'controller' => 'indexController',
@@ -36,26 +26,6 @@
   	}
 
 
-  	public function run($url) {
-  		
-  		foreach ($this->getRoutes() as $key => $route) {
-  			if($url == $route['route']) {
-  				$class = "App\\Controllers\\". ucfirst($route['controller']); 
-
-  				$controller = new $class;
-
-  				$action = $route['action'];
-
-  				$controller->$action();
-
-  			}
-  		}
-
-  	}
-
-  	public function getUrl() {
-  		return parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-  	}
 
   }
 
